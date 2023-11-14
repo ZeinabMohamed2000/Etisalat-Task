f91b24f95d6ff3520f3093f64bbcb5050ff4a945
ZeinabMohamed <zm4816035@gmail.com>
2023-11-14 14:07:03 -0800

    first commit

---
+++ Etisalat TaskUITests/Etisalat_TaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Etisalat_TaskUITestsLaunchTests.swift
+//  Etisalat TaskUITests
+//
+//  Created by Zeinab on 14/11/2023.
+//
+
+import XCTest
+
+final class Etisalat_TaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
